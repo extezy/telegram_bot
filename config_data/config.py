97a4ae595e58453b2e8801aad01026aaf39285e6
Viachaslau <slava@informatics.by>
2022-07-22 22:48:01 +0300

    config with main directory files

--- config_data/config.py
+++ config_data/config.py
@@ -1,14 +1,24 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
+
 if not find_dotenv():
     exit('Переменные окружения не загружены т.к отсутствует файл .env')
 else:
     load_dotenv()
 
+ROOT_DIR = os.path.dirname(os.path.abspath('__main__'))
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+DATABASE = os.path.join(ROOT_DIR, 'database', 'history.db')
+MAX_HOTELS_COUNT = 10
+MAX_PHOTO_COUNT = 10
+
 DEFAULT_COMMANDS = (
+    ('lowprice', "Топ дешевых отелей"),
+    ('highprice', "Топ дорогих отелей"),
+    ('bestdeal', "Топ подходящих отелей"),
+    ('history', "История поиска"),
     ('start', "Запустить бота"),
-    ('help', "Вывести справку")
+    ('help', "Вывести справку"),
 )
--- loader.py
+++ loader.py
@@ -1,7 +1,11 @@
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
+from peewee import SqliteDatabase
+from telebot_calendar import Calendar, RUSSIAN_LANGUAGE, CallbackData
+
 
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
-
+database = SqliteDatabase(config.DATABASE)
+calendar_in_message = Calendar(language=RUSSIAN_LANGUAGE)
